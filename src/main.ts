e424c165c17b1a208e2a205deb1cfe795c8204c3
PhilipOyelegbin <info@philipoyelegbin.com.ng>
2025-10-02 08:06:39 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as serverless from 'serverless-http';
+// import serverless = require('serverless-http');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    bodyParser: true,
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:3030',
+        'https://philipoyelegbin.com.ng',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
+
+  // const globalPrefix = '.netlify/functions/main';
+  app.setGlobalPrefix('/api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, {
+      deepScanRoutes: true,
+    });
+  SwaggerModule.setup('/', app, documentFactory);
+
+  // await app.listen(process.env.PORT ?? 3000);
+  // console.log(`Application is running...`);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp);
+}
+
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
